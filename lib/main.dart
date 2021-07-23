284eb791463ea3e936d79918d97ed16d54fe1cea
martinindra <martin.indra19@gmail.com>
2021-07-23 10:57:21 +0700

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'main_screen.dart';
+import 'detail_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: MainScreen(),
+    );
+  }
+}
